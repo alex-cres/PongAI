d5e2c637d9d0a6d184f263e907695861e439f84a
alex-cres <alexrealinho@gmail.com>
2021-04-03 13:03:28 +0100

    Added AI and difficulties of the AI

--- Pong.py
+++ Pong.py
@@ -3,6 +3,7 @@ from playsound import playsound
 import os 
 import threading
 import random
+import math
 
 #sound threading for async sound
 def soundPlayer(soundFile):
@@ -18,6 +19,9 @@ score_base_height=(height/2)-40
 randList = [-1,1]
 width_limits=[-width_perimeter,width_perimeter]
 height_limits=[score_base_height-10,-height_perimeter]
+robotCycleMovement=10
+robotCycle=0
+
         
 color="black"
 #defining a screen, title, backgound color, size
@@ -30,12 +34,13 @@ wn.tracer(0)
 
 #Paddle class
 class Paddle:
-    def __init__(self,controls,starting_width,stretch_wid,stretch_len):
+    def __init__(self,controls,starting_width,stretch_wid,stretch_len,color="white",typePaddle=1,speedRobot=0):
+        self.typePaddle=typePaddle
         self.paddle_turtle = turtle.Turtle()
         self.paddle_turtle.st()
         self.paddle_turtle.speed(0) #Animation speed
         self.paddle_turtle.shape("square") # defult 20x20
-        self.paddle_turtle.color("white")
+        self.paddle_turtle.color(color)
         self.paddle_turtle.shapesize(stretch_wid=stretch_wid,stretch_len=stretch_len) # modify the size x times
         self.paddle_turtle.penup()
         self.paddle_turtle.goto(starting_width,0)
@@ -43,6 +48,7 @@ class Paddle:
         self.stretch_wid = stretch_wid
         self.stretch_len = stretch_len
         self.controls = controls
+        self.speedRobot=speedRobot
     def paddle(self,control):
         y = self.paddle_turtle.ycor()
         #if y + (self.stretch_wid*10) <= width/2 and y - (self.stretch_wid*10) >= -width/2:
@@ -63,12 +69,26 @@ class Paddle:
         else:
             return turtle_object.xcor() > (self.starting_width-10) and turtle_object.xcor()  < self.starting_width
     def afterCollisionXPosition(self):
-        if self.starting_width <0:
+        if self.starting_width < 0:
             return (self.starting_width+10)
         else:
             return (self.starting_width-10)
     def clear(self):
         self.paddle_turtle.ht()
+    def isRobot(self):
+        return self.typePaddle!=1
+    def movementRobot(self,ball_turtle,adversary_turtle,height_limits):
+        y = self.paddle_turtle.ycor()
+        if y+(self.stretch_wid)*10  > height_limits[1]:
+            y=height_limits[1]-(self.stretch_wid)*10
+        elif y-(self.stretch_wid)*10 < height_limits[0]:
+            y=height_limits[0]+(self.stretch_wid)*10
+        elif ball_turtle.ycor() < y:
+            y-= self.speedRobot
+        elif ball_turtle.ycor() > y:
+            y+= self.speedRobot
+        self.paddle_turtle.sety(y)
+
 
 #Score class   
 class Score:
@@ -153,9 +173,11 @@ class Ball:
     def sety(self,y):
         self.ball.sety(y)
     def xcor(self):
-        self.ball.xcor()
+        return self.ball.xcor()
     def ycor(self):
-        self.ball.ycor()
+        return self.ball.ycor()
+    def dy(self):
+        return self.ball.dy
     def moveBall(self):
         self.ball.setx(self.ball.xcor()+self.ball.dx)
         self.ball.sety(self.ball.ycor()+self.ball.dy)
@@ -194,12 +216,14 @@ class Ball:
 
 class Game:
     def __init__(self):
+        self.typeGame=1
         self.isInMenu=True
         self.isInGame=False
         self.menu={}
         self.menu["objects"]=[]
         self.game={}
         self.game["objects"]=[]
+        self.speedRobot = 0.1
     def main(self):
         while self.isInMenu:
             self.startMenu()
@@ -221,8 +245,8 @@ class Game:
         pen.color("white")
         pen.penup()
         pen.hideturtle()
-        pen.goto(0,height)
-        pen.write(text,align="center",font=("Courier",24,"normal"))
+        pen.goto(-(width/2-width/12),height)
+        pen.write(text,font=("Courier",24,"normal"))
         return pen
     def startMenu(self):
         self.isInMenu=True
@@ -231,21 +255,36 @@ class Game:
         self.menu["objects"].append(Line(-width_perimeter,score_base_height-10).writeDashed(height,40,18))
         self.menu["objects"].append(Line(-width_perimeter,score_base_height-10,1).HorizontalToRight().writeDashed(width,40,20))
         self.menu["objects"].append(Line(-width_perimeter,-height_perimeter,1).HorizontalToRight().writeDashed(width,40,20))
-        self.menu["objects"].append(self.drawMenuItem("   1. Enter Game",score_base_height-score_base_height*5/10))
-        self.menu["objects"].append(self.drawMenuItem("Esc. Exit Game",score_base_height-score_base_height*7/10))
+        self.menu["objects"].append(self.drawMenuItem("  1. Enter 2v2 Game",score_base_height-score_base_height*4/10))
+        self.menu["objects"].append(self.drawMenuItem("  2. Enter vs AI Game 0.3",score_base_height-score_base_height*6/10))
+        self.menu["objects"].append(self.drawMenuItem("  3. Enter vs AI Game 0.4",score_base_height-score_base_height*8/10))
+        self.menu["objects"].append(self.drawMenuItem("  4. Enter vs AI Game 0.5",score_base_height-score_base_height*10/10))
+        self.menu["objects"].append(self.drawMenuItem("  5. Enter vs AI Game 0.6",score_base_height-score_base_height*12/10))
+        self.menu["objects"].append(self.drawMenuItem("  6. Enter vs AI Game 0.7",score_base_height-score_base_height*14/10))
+        self.menu["objects"].append(self.drawMenuItem("  7. Enter vs AI Game 0.8",score_base_height-score_base_height*16/10))
+        self.menu["objects"].append(self.drawMenuItem("  8. Enter vs AI Game 0.9",score_base_height-score_base_height*18/10))
+        self.menu["objects"].append(self.drawMenuItem("Esc. Exit Game",score_base_height-score_base_height*20/10))
         wn.listen()
         wn.onkeypress(lambda:self.exitMenu(),"Escape")
-        wn.onkeypress(lambda:self.enterGame(),"1")
+        wn.onkeypress(lambda:self.enterGame(0  ,1),"1")
+        wn.onkeypress(lambda:self.enterGame(0.3,2),"2")
+        wn.onkeypress(lambda:self.enterGame(0.4,2),"3")
+        wn.onkeypress(lambda:self.enterGame(0.5,2),"4")
+        wn.onkeypress(lambda:self.enterGame(0.6,2),"5")
+        wn.onkeypress(lambda:self.enterGame(0.7,2),"6")
+        wn.onkeypress(lambda:self.enterGame(0.8,2),"7")
+        wn.onkeypress(lambda:self.enterGame(0.9,2),"8")
         while self.isInMenu:
             wn.update() #updates the screen
-    def enterGame(self):
+    def enterGame(self,speedRobot,typeGame=1):
         self.exitMenu()
         self.isInGame=True
+        self.typeGame=typeGame
+        self.speedRobot = speedRobot
     def exitMenu(self):
         self.isInMenu=False
         for i in self.menu["objects"]:
             i.clear()
-
     def startGame(self):
         #--------------------------#-----------------------------------#
         wn.bgcolor(color)
@@ -256,6 +295,7 @@ class Game:
         starting_width_paddles= width/2 -width/16
         width_limits=[-width_perimeter,width_perimeter]
         height_limits=[-height_perimeter,score_base_height-10]
+        robotCycle=0
         #--------------------------#-----------------------------------#
         #Init Lines
         self.game["objects"].append(Line(0,score_base_height-10).writeDashed(height,20))
@@ -266,7 +306,7 @@ class Game:
         
         #Init Paddles
         paddle_a = Paddle(["w","s"],-starting_width_paddles,5,1)
-        paddle_b = Paddle(["Up","Down"],+starting_width_paddles,5,1)
+        paddle_b = Paddle(["Up","Down"],+starting_width_paddles,5,1,"white" if self.typeGame==1 else "red",self.typeGame,self.speedRobot)
         self.game["objects"].append(paddle_a)
         self.game["objects"].append(paddle_b)
         #Init Score 
@@ -278,19 +318,26 @@ class Game:
         #Keyboard binding
         wn.listen()
         paddle_a.key_bind()
-        paddle_b.key_bind()
+        if not paddle_b.isRobot():
+            paddle_b.key_bind()
         wn.onkeypress(lambda:self.exitGame(),"Escape")
         #Main game loop
         while self.isInGame:
             wn.update() #updates the screen
             #Move ball
             ball.moveBall()
+            #Move AI
+            if paddle_b.isRobot():
+                if robotCycle >= robotCycleMovement:
+                    paddle_b.movementRobot(ball,paddle_a,height_limits)
+                    robotCycle=0
+                else:
+                    robotCycle+=1
             #Border checking
             ball.checkWalls(score)
             #Collision Between ball and the paddles
             ball.checkCollision(paddle_b)
             ball.checkCollision(paddle_a)
-
     def exitGame(self):
         self.isInGame=False
         self.isInMenu=True
